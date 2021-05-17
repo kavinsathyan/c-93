8a0f1fc1f6d486479c6443ed1b09241697ebeeb4
kavinsathyan <79074982+kavinsathyan@users.noreply.github.com>
2021-05-17 15:52:15 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,47 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+var firebaseConfig = {
+      apiKey: "AIzaSyDXyJih_QJamc7KFfU6bVYiz633dlsnD1Q",
+      authDomain: "kwitter-app-96c6a.firebaseapp.com",
+      projectId: "kwitter-app-96c6a",
+      storageBucket: "kwitter-app-96c6a.appspot.com",
+      messagingSenderId: "644020362086",
+      appId: "1:644020362086:web:92cec17759fa73504006a7"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    user_name = localStorage.getItem("user_name");
+
+    document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+    function addRoom()
+    {
+          room_name = document.getElementById("room_name").value;
+
+          firebase.database().ref("/").child(room_name).update({
+                purpose : "adding room name"
+          });
+
+          localStorage.setItem("room_name", room_name);
+
+          window.location = "kwitter_page.html";
+    }
+
+
+    function getData() 
+{firebase.database().ref("/").on('value', 
+function(snapshot) 
+{document.getElementById("output").innerHTML = "";
+snapshot.forEach(function(childSnapshot) 
+{
+      childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
+
+
+
